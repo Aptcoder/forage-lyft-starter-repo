dc6e3758ac51f7c81c26259a78b8a27309f5f59f
Samuel <omilosamuel@gmail.com>
2022-08-20 11:42:25 +0100

    separate car_factory into file

--- car.py
+++ car.py
@@ -1,10 +1,5 @@
 from abc import ABC, abstractmethod
 
-from .engine.sternman_engine import SternmanEngine
-
-from .engine.willoughby_engine import WilloughbyEngine
-
-from .engine.capulet_engine import CapuletEngine
 from .engine.engine_interface import Engine
 
 class Serviceable(ABC):
@@ -21,28 +16,3 @@ class Car(Serviceable):
         pass
 
 
-class CarFactory:
-    @staticmethod
-    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
-        capulet_engine = CapuletEngine(current_mileage, last_service_mileage)
-        return Car(capulet_engine, last_service_date)
-    
-    @staticmethod
-    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
-        willoughby_engine = WilloughbyEngine(current_mileage, last_service_mileage)
-        return Car(willoughby_engine, last_service_date)
-    
-    @staticmethod
-    def create_palindrome(current_date, last_service_date, warning_light_on) -> Car:
-        stenman_engine = SternmanEngine(warning_light_on)
-        return Car(stenman_engine, last_service_date)
-    
-    @staticmethod
-    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
-        willoughby_engine = WilloughbyEngine(current_mileage, last_service_mileage)
-        return Car(willoughby_engine, last_service_date)
-
-    @staticmethod
-    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
-        capulet_engine = CapuletEngine(current_mileage, last_service_mileage)
-        return Car(capulet_engine, last_service_date)
